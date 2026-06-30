6d76c7be2c89e673b86a5c39da4130522a9e0ea5
Khoi Ha <hakhoi@gmx.de>
2026-06-30 17:28:01 +0200

    🧹 Platzhalterdateien entfernen
    Leere Swift-Dateien erzeugen nur Rauschen im Projekt und suggerieren unfertige Architekturbausteine. Der MVP bleibt dadurch schlanker und klarer überprüfbar.

--- SwipeBeats/SwipeBeats/Resources/PreviewData.swift
+++
@@ -1,7 +0,0 @@
-//
-//  PreviewData.swift
-//  SwipeBeats
-//
-//  Created by Vu Minh Khoi Ha on 21.01.26.
-//
-
--- SwipeBeats/SwipeBeats/Shared/UIComponents/PrimaryButton.swift
+++
@@ -1,7 +0,0 @@
-//
-//  PrimaryButton.swift
-//  SwipeBeats
-//
-//  Created by Vu Minh Khoi Ha on 21.01.26.
-//
-
